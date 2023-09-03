044f3c2d7434e4cd37865f40a53e012327d65a1a
Matheus FErreira <matheus.ferreira165@outlook.com>
2023-09-03 17:14:10 -0300

    testing db and handlefunc

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"net/http"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
 func main() {
 
+	conn, err := gorm.Open(postgres.Open(""), &gorm.Config{})
+
+	if err != nil {
+		panic("could not connect to the database")
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World"))
+	})
+
+	http.ListenAndServe(":3000", nil)
 }
