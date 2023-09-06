98ed1ddba082215b898d772e7167ac1f172406f7
Matheus FErreira <matheus.ferreira165@outlook.com>
2023-09-05 23:18:13 -0300

    feat: add enviroment variables

--- database/connection.go
+++ database/connection.go
@@ -1,6 +1,11 @@
 package database
 
 import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 	"github.com/matheusferreira165/jwt-authentication/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -10,7 +15,21 @@ var DB *gorm.DB
 
 func Connect() {
 
-	conn, err := gorm.Open(postgres.Open("postgres://postgres:develope123@localhost:5432/develope_db"), &gorm.Config{})
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Erro ao carregar o arquivo .env")
+	}
+
+	// Recupera as variáveis de ambiente
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+
+	// Constrói a string de conexão com o banco de dados
+	dns := fmt.Sprintf("host=%s port=%s dbname=%s user=%s password=%s sslmode=disable", dbHost, dbPort, dbName, dbUser, dbPassword)
+
+	conn, err := gorm.Open(postgres.Open(dns), &gorm.Config{})
 
 	if err != nil {
 		panic("could not connect to the database")
